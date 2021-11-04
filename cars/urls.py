f2d5883b3edaeb80adf6f6ef7b5bfa91cdf34634
atabekdemurtaza <mychannel21war@gmail.com>
2021-11-05 03:45:42 +0500

    Adding car details

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 from .views import cars 
+from .views import car_detail
 
 urlpatterns = [
 	
 	path('', cars, name='cars'),
+	path('<int:id>', car_detail, name='car_detail'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -27,3 +27,4 @@ def services(request):
 def contact(request):
 
 	return render(request, 'pages/contact.html')
+
