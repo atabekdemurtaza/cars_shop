60674a2cb71ffa34e838945c168528b2b2b432d5
atabekdemurtaza <mychannel21war@gmail.com>
2021-11-14 05:03:04 +0500

    has changed settings and search

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from cars.views import search
 from django.shortcuts import render
 from .models import Team
 from cars.models import Car
@@ -6,10 +7,21 @@ def home(request):
 	teams = Team.objects.all()
 	featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
 	all_cars = Car.objects.order_by('-created_date')
+	#search_fields = Car.objects.values('model', 'city', 'body_style', 'year')
+
+	model_search = Car.objects.values_list('model', flat=True).distinct() #These 4 lines of code are not dictionary
+	city_search = Car.objects.values_list('city', flat=True).distinct()   #They are only lists
+	body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+	year_search = Car.objects.values_list('year', flat=True).distinct()
 	data = {
 		'teams': teams,
 		'featured_cars': featured_cars,
 		'all_cars': all_cars,
+		#'search_fields': search_fields,
+		'model_search': model_search,
+		'city_search': city_search,
+		'body_style_search': body_style_search,
+		'year_search': year_search,
 	}
 	return render(request, 'pages/home.html', data)
 
